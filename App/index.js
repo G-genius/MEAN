ea83d98efc6d33ead0b3f39c3dcb7e611f25f71d
G-genius <gamelob25@gmail.com>
2022-11-17 12:13:43 +0300

    Подключение к mongodb
    2 видео

---
+++ App/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+const config = require("./config/db")
+
+const app = express()
+const port = 3000
+
+mongoose.connect(config.db, {useNewUrlParser: true})
+//Если подключение успешно
+mongoose.connection.on("connected", () => {
+    console.log("Успешное подключение к mongodb")
+})
+//Если не успешно
+mongoose.connection.on("error", (err) => {
+    console.log("Ошибка подключения к mongodb " + err)
+})
+
+app.listen(port, () => {
+    console.log("Сервер был запущен по порту " + port)
+})
+
+app.get("/", (req, res) => {
+    res.send("Главная страница сайта")
+})
