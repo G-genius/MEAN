b06edde2e7be8816c102dc72addc7a2097e0dc30
G-genius <gamelob25@gmail.com>
2022-11-17 14:19:48 +0300

    Создание модели пользователя
    4 видео

--- App/routes/account.js
+++ App/routes/account.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router()
+const User = require("../models/user")
 
-router.get("/reg", (req, res) => {
-    res.send("Регистрация")
+router.post("/reg", (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        login: req.body.login,
+        password: req.body.password
+    })
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: "Пользователь не добавлен"
+            })
+        }
+        else {
+            res.json({
+                success: true,
+                msg: "Пользователь успешно добавлен"
+            })
+        }
+    })
 })
 
 router.get("/auth", (req, res) => {
